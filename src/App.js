0f6c1cf2f967a4fe779c70e5ea137f15f810ad29
firas hamila <firas.hm.1996@gmail.com>
2024-02-27 18:33:31 +0100

    ss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Login from "./Components/Login/Login";
 import Home from "./Components/Home/Home";
@@ -7,14 +7,21 @@ import MainHeader from "./Components/MainHeader/MainHeader";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token == 1) {
+      setIsLoggedIn(true);
+    }
+    console.log("aaa");
+  }, []);
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
+    localStorage.setItem("token", 1);
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.removeItem("token");
   };
 
   return (
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -11,20 +11,36 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  useEffect(() => {
+    setFormIsValid(emailIsValid && passwordIsValid);
+  }, [emailIsValid, passwordIsValid]);
+  useEffect(() => {
+    // debouncing
+    const timer = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+    // console.log("effect");
+    return () => {
+      // console.log("clean-up");
+      clearTimeout(timer);
+    };
+  }, [enteredEmail, enteredPassword]);
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
 
-    setFormIsValid(
-      event.target.value.includes("@") && enteredPassword.trim().length > 6
-    );
+    // setFormIsValid(
+    //   event.target.value.includes("@") && enteredPassword.trim().length > 6
+    // );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
 
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes("@")
-    );
+    // setFormIsValid(
+    //   event.target.value.trim().length > 6 && enteredEmail.includes("@")
+    // );
   };
 
   const validateEmailHandler = () => {
